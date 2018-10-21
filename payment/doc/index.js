74502f2806fb7efd3f628889807b6a5b5dff8734
Hiroaki KARASAWA <siqvare@gmail.com>
2018-10-21 13:34:29 +0900

    Implemnet doc root

--- payment/doc/index.js
+++ payment/doc/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 var path    = require("path");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`
+    <ul>
+        <li><a href="/payment">Payment API</a></li>
+        <li><a href="/store">Store API</a></li>
+        <li><a href="/frontend">Frontend API</a></li>
+    </ul>
+`));
 
 app.get('/payment', (req, res) => {
     res.sendFile(path.join(__dirname, 'payment', 'v1', 'api.html'));
