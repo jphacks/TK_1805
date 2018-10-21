2d7e84209d7df4e1658fcd7aedc06fcdf32a3188
Hiroaki KARASAWA <siqvare@gmail.com>
2018-10-21 13:31:07 +0900

    Add store and frontend api

--- payment/doc/index.js
+++ payment/doc/index.js
@@ -5,7 +5,15 @@ var path    = require("path");
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/payment', (req, res) => {
-    res.sendFile(path.join(__dirname+'/v1/api.html'));
+    res.sendFile(path.join(__dirname, 'payment', 'v1', 'api.html'));
+});
+
+app.get('/store', (req, res) => {
+    res.sendFile(path.join(__dirname, 'store', 'v1', 'api.html'));
+});
+
+app.get('/frontend', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'api.html'));
 });
 
 app.listen(3000, () => console.log('Example express app listening on port 3000!'));
