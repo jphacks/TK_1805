ef5126e2a16973caa6258aef3bb035111c61e255
KeisukeYamashita <19yamashita15@gmail.com>
2018-10-28 02:49:24 +0900

    add get paymentURL test

--- store/helpers/helpers_test.go
+++ store/helpers/helpers_test.go
@@ -1,1 +1,43 @@
 package helpers
+
+import (
+	"os"
+	"strconv"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const (
+	paymentHost string = "hahaKoregaTest"
+	paymentPort int    = 9999
+)
+
+func _Setenv() {
+	os.Setenv("PAYMENT_HOST", paymentHost)
+	os.Setenv("PAYMENT_PORT", strconv.Itoa(paymentPort))
+}
+
+func TestGetPaymentServerInfo(t *testing.T) {
+	Convey("GetPaymentServerInfo is", t, func() {
+		Convey("when debug, dev, test mode", func() {
+			var testBool bool = true
+			_Setenv()
+
+			host, port, err := GetPaymentServerInfo(testBool)
+			So(err, ShouldBeNil)
+			So(host, ShouldEqual, "http://localhost")
+			So(port, ShouldEqual, 8880)
+		})
+
+		Convey("when prod mode", func() {
+			var testBool bool = false
+			_Setenv()
+
+			host, port, err := GetPaymentServerInfo(testBool)
+			So(err, ShouldBeNil)
+			So(host, ShouldEqual, "http://"+paymentHost)
+			So(port, ShouldEqual, paymentPort)
+		})
+	})
+}
