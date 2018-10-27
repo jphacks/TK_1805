4a65c5cefe0cb81180641260f0fb1924e36b7cb7
Hiroaki KARASAWA <siqvare@gmail.com>
2018-10-27 21:21:57 +0900

    Add routing on express

--- frontend/scripts/start-prod.js
+++ frontend/scripts/start-prod.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 app.use(express.static('build'));
+
+app.get('/tables*', (req, res, next) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '../build') });
+});
 
 app.listen(3000, () => console.log('Frontend app is listening on port 3000!'));
