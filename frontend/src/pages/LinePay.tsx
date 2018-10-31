50e4a11dd5e1697b9e3a4161fa2bae6c6e2f1b49
Hiroaki KARASAWA <siqvare@gmail.com>
2018-10-31 20:18:52 +0900

    Implement line pay

--- frontend/src/pages/LinePay.tsx
+++ frontend/src/pages/LinePay.tsx
@@ -4,11 +4,14 @@ import styled from 'styled-components';
 import { Order } from '../types/order';
 import Initializer from '../components/Initializer';
 import Header from '../components/Header';
+import { STORE_API_URL_BASE } from '../config/api';
+import Amount from '../components/Amount';
 
 type Props = {
   itemMap: any,
   orders: Order[],
   groupId: string,
+  storeName: string,
   uid: string,
   match: any,
   history: any,
@@ -19,14 +22,14 @@ type Props = {
   orders: order.orders,
   uid: order.uid,
   groupId: store.groupId,
+  storeName: store.name,
 }))
 @observer
 export default class LinePay extends React.Component<Props> {
   state = {
-    number: '',
-    exp_month: 11,
-    exp_year: 2018,
-    cvc: '',
+    paymentURL: '',
+    error: '',
+    done: false,
   };
 
   get amount() {
@@ -41,16 +44,106 @@ export default class LinePay extends React.Component<Props> {
     }, 0);
   }
 
+  async componentDidUpdate() {
+    if (this.props.groupId === '' || this.amount=== 0 || this.state.done) {
+      return;
+    } else {
+      this.setState({ done: true });
+    }
+
+    try {
+      const response = await fetch(`${STORE_API_URL_BASE}/linepay/reserve`, {
+        method: 'POST',
+        body: JSON.stringify({
+          orderId: this.props.groupId,
+          amount: this.amount,
+          item: `[OAISO] ${this.props.storeName}でのお支払い`,
+          redirectUrl: 'https://oaiso.tk/thankyou',
+        })
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (data.error) {
+        this.setState({ error: 'サーバーで問題が発生したため決済に進めません。他の決済方法をお試しください。' });
+      } else {
+        this.setState({ paymentURL: data.message.paymentURL });
+      }
+    } catch(e) {
+      console.error(e);
+      this.setState({ error: 'サーバーで問題が発生したため決済に進めません。他の決済方法をお試しください。' });
+    }
+  }
+
   render() {
     return (
-      <main>
+      <Main>
         <Initializer match={this.props.match} />
         <Header title='LINE Pay' history={this.props.history} />
 
-        <div style={{ height: 200, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          TODO: 必ず実装する!
-        </div>
-      </main>
+        <Amount amount={this.amount} style={{ marginBottom: 20 }} />
+
+        { this.props.orders.length === 0
+        ?
+          <Description>
+            まだ注文がありません。
+          </Description>
+        :
+          this.state.error
+        ?
+          <Error>
+            { this.state.error }
+          </Error>
+        :
+          this.state.paymentURL === ''
+        ?
+          <Description>
+            準備中
+          </Description>
+        :
+          [
+            <Description key={0}>
+              準備が整いました。下のリンクからLINE Payのページに移動し決済を行ってください。
+            </Description>,
+            <PayButton key={1} href={this.state.paymentURL}>
+              続ける
+            </PayButton>
+          ]
+        }
+      </Main>
     );
   }
 }
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Description = styled.div`
+  width: 80%;
+  font-weight: bold;
+`;
+
+const Error = styled.div`
+  width: 80%;
+  color: red;
+  font-weight: bold;
+`;
+
+const PayButton = styled.a`
+  color: #00c300;
+  background-color: white;
+  border: 1px solid #00c300;
+  border-radius: 4px;
+  margin-top: 40px;
+  padding: 10px 60px;
+  text-decoration: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
